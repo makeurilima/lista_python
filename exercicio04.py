78a6e555420e28df7e020945bdf78e67fe64e2c9
Makeuri de Lima Louzeiro <makeurilima03@gmail.com>
2025-09-11 20:05:24 -0300

    resolução do exercício 04

---
+++ exercicio04.py
@@ -0,0 +1,10 @@
+# Exercício 4: Armazenar e exibir ano e Pi
+# Criamos uma variável para um número inteiro (int) e outra para um número de ponto flutuante (float).
+ano_atual = 2025
+valor_pi = 3.14
+
+# Exibimos os valores de ambas as variáveis.
+print(f"Ano Atual: {ano_atual}")
+print(f"Valor de Pi: {valor_pi}")
+
+print("-" * 20)
