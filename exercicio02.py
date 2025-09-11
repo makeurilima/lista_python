ef6d8cc140b507d010d40969c3c37cd2e00a0008
Makeuri de Lima Louzeiro <makeurilima03@gmail.com>
2025-09-11 20:02:19 -0300

    resolução de exercício 02

---
+++ exercicio02.py
@@ -0,0 +1,13 @@
+# Exercício 2: Criar e exibir variáveis
+# Variáveis são usadas para armazenar dados.
+# Aqui, criamos três variáveis e atribuímos valores a elas.
+nome = "Ada Lovelace"
+profissao = "Programadora"
+idade = 36
+
+# Exibimos o valor de cada variável em uma linha separada.
+print(nome)
+print(profissao)
+print(idade)
+
+print("-" * 20)
