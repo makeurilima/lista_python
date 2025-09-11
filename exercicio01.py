be73ece3ae3ddd6d610b6f00003b17f2899afa58
Makeuri de Lima Louzeiro <makeurilima03@gmail.com>
2025-09-11 20:00:44 -0300

    resolução de exercício 01

---
+++ exercicio01.py
@@ -0,0 +1,6 @@
+# Exercício 1: Exibir uma mensagem
+# A função print() é usada para exibir informações na tela.
+# O texto (string) que queremos exibir é colocado entre aspas.
+print("Eu amo programar em Python!")
+
+print("-" * 20) # Separador para facilitar a leitura
