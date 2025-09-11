7eec2b1c7f53910e38f191e73510e8b44786063f
Makeuri de Lima Louzeiro <makeurilima03@gmail.com>
2025-09-11 20:03:48 -0300

    resolução do exercício 03

---
+++ exercicio03.py
@@ -0,0 +1,6 @@
+# Exercício 3: Concatenar informações em uma frase
+# Usamos uma f-string (string formatada) para combinar texto e variáveis em uma única mensagem.
+# As variáveis são colocadas dentro de chaves {}.
+print(f"Nome: {nome}, Profissão: {profissao}, Idade: {idade} anos.")
+
+print("-" * 20)
