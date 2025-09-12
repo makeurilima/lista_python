54960fc59c8d7e41cec07a5de4ab50555981a32b
Makeuri de Lima Louzeiro <makeurilima03@gmail.com>
2025-09-11 21:52:12 -0300

    resolução do exercício 08

---
+++ exercicio08.py
@@ -0,0 +1,8 @@
+# Exercício 8: Hobby favorito
+# Armazenamos o hobby do usuário em uma variável.
+hobby_favorito = input("Qual é o seu hobby favorito? ")
+
+# Exibimos uma frase com a informação.
+print(f"Seu hobby favorito é {hobby_favorito}. Que legal!")
+
+print("-" * 20)
