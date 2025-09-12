44f3a903956639528e80cf3fb8b5beb9f23a4d02
Makeuri de Lima Louzeiro <makeurilima03@gmail.com>
2025-09-11 21:59:50 -0300

    resolução do exercício 15

---
+++ exercicio15.py
@@ -0,0 +1,12 @@
+# Exercício 15: Calcular área de um retângulo
+# A fórmula da área é largura * altura.
+largura = float(input("Digite a largura do retângulo: "))
+altura = float(input("Digite a altura do retângulo: "))
+
+# Calculamos a área.
+area = largura * altura
+
+# Exibimos o resultado.
+print(f"A área de um retângulo com largura {largura} e altura {altura} é {area}.")
+
+print("-" * 20)
