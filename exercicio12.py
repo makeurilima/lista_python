60334e7c506fdc196b4a1660f860ff1ca5d37cf9
Makeuri de Lima Louzeiro <makeurilima03@gmail.com>
2025-09-11 21:56:40 -0300

    resolução do exercício 12

---
+++ exercicio12.py
@@ -0,0 +1,13 @@
+# Exercício 12: Dividir dois números
+# Usamos float() para converter os inputs, pois a divisão pode resultar em um número decimal.
+dividendo = float(input("Digite o primeiro número (dividendo): "))
+divisor = float(input("Digite o segundo número (divisor): "))
+
+# Realizamos a divisão.
+resultado_divisao = dividendo / divisor
+
+# Exibimos o resultado.
+print(f"A divisão de {dividendo} por {divisor} é igual a {resultado_divisao}.")
+
+print("-" * 20)
+
