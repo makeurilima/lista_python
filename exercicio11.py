c64b46c8a63af1ba263ef96d400c62004e74dfc5
Makeuri de Lima Louzeiro <makeurilima03@gmail.com>
2025-09-11 21:55:43 -0300

    resolução do exercício 11

---
+++ exercicio11.py
@@ -0,0 +1,11 @@
+# Exercício 11: Multiplicar número por 5
+# Pedimos um número e o convertemos para inteiro.
+numero_para_multiplicar = int(input("Digite um número inteiro: "))
+
+# Calculamos o resultado da multiplicação.
+resultado_multiplicacao = numero_para_multiplicar * 5
+
+# Mostramos o resultado.
+print(f"O resultado de {numero_para_multiplicar} multiplicado por 5 é {resultado_multiplicacao}.")
+
+print("-" * 20)
