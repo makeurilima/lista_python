af5b854053620a428f0bfb6337a019e0e4e60223
Makeuri de Lima Louzeiro <makeurilima03@gmail.com>
2025-09-11 21:50:00 -0300

    resolução do exercício 07

---
+++ exercicio07.py
@@ -0,0 +1,9 @@
+# Exercício 7: Perguntar cidade e estado
+# Usamos a função input() duas vezes para obter informações diferentes.
+cidade_natal = input("Em qual cidade você nasceu? ")
+estado_natal = input("E em qual estado? ")
+
+# Exibimos uma frase com as duas informações.
+print(f"Você nasceu em {cidade_natal}, {estado_natal}.")
+
+print("-" * 20)
