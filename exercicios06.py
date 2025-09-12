030e9067bbf031317937ac2f4f23137952b5cdc0
Makeuri de Lima Louzeiro <makeurilima03@gmail.com>
2025-09-11 21:45:52 -0300

    resolução do exercício 06

---
+++ exercicios06.py
@@ -0,0 +1,9 @@
+# Exercício 6: Mensagem de boas-vindas com input
+# A função input() pausa o programa e espera que o usuário digite algo.
+# O valor digitado é armazenado na variável 'nome_usuario'.
+nome_usuario = input("Por favor, digite o seu nome: ")
+
+# Exibimos uma mensagem de boas-vindas usando o nome fornecido.
+print(f"Olá, {nome_usuario}! Seja bem-vindo(a)!")
+
+print("-" * 20)
