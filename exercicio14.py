d4769e8b49519a8f7f548cbf9be83deae538e088
Makeuri de Lima Louzeiro <makeurilima03@gmail.com>
2025-09-11 21:58:56 -0300

    resolução do exercício 14

---
+++ exercicio14.py
@@ -0,0 +1,12 @@
+# Exercício 14: Converter metros para centímetros
+# 1 metro é igual a 100 centímetros.
+valor_metros = float(input("Digite um valor em metros: "))
+
+# Realizamos a conversão.
+valor_centimetros = valor_metros * 100
+
+# Exibimos o resultado da conversão.
+print(f"{valor_metros} metro(s) equivale(m) a {valor_centimetros} centímetros.")
+
+print("-" * 20)
+
