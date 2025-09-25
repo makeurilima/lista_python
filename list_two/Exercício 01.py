56918687612e5ed653e538eeebb41def10ef9760
Makeuri Louzeiro <louzeiromakeuri@gmail.com>
2025-09-25 19:44:40 -0300

    Resolução do exercício 1

---
+++ list_two/Exercício 01.py
@@ -0,0 +1,12 @@
+# Crie uma função que receba dois números e retorne a soma deles.
+
+def soma_algoritmos(a,b):
+    return a+b
+algoritmo_a = int(input('Digite o primeiro valor aqui: '))
+algoritmo_b = int(input('Digite o segundo valor aqui: '))
+soma = algoritmo_a + algoritmo_b
+
+print (f'O resultado da soma entre {algoritmo_a} e {algoritmo_b} é {soma} ')
+
+
+
