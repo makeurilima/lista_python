74aca24f4d47c655e8ba9a5cfe715ee351d09e40
Makeuri Louzeiro <louzeiromakeuri@gmail.com>
2025-09-25 19:44:56 -0300

    Resolução do exercício 2

---
+++ list_two/Exercício 02.py
@@ -0,0 +1,12 @@
+#Crie uma função que receba dois números e retorne o resultado da multiplicação.
+
+def multiplicacao_algoritmos(a, b):
+    return a * b
+
+primeiro_algoritmo = int(input('Digite o primeiro algoritimo: '))
+segundo_algoritmo = int(input('Digite o segundo algoritimo: '))
+multiplicacao = primeiro_algoritmo * segundo_algoritmo
+
+print(f'O resultado da multiplicação entre {primeiro_algoritmo} e {segundo_algoritmo} é {multiplicacao}.')
+
+
